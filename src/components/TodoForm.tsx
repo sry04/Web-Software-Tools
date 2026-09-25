a84fcc37311e2ca3916b03c47afc318f0fcdbdcf
suryana <suryanauya04@gmail.com>
2026-09-25 20:37:52 +0700

    feat: add todo list (ilham restu)

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function TodoForm({ addTodo }) {
+function TodoForm({ addTodo: any }) {
   const [todo, setTodo] = useState("");
 
   const handleSubmit = (e) => {
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,13 @@
+function TodoList({ todos }) {
+  return (
+    <ul>
+      {todos.map((todo, index) => (
+        <li key={index}>
+          {todo}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
