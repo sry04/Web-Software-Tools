fffa13db8d20146f6ff954440237eb0f20ecec89
awalramadan041102-glitch <awalramadan041102@gmail.com>
2026-09-25 20:31:58 +0700

    feat: add todo form

---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function TodoForm({ addTodo }) {
+  const [todo, setTodo] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (todo.trim() === "") return;
+
+    addTodo(todo);
+    setTodo("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Ketik tugas..."
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+
+      <button type="submit">Tambah</button>
+    </form>
+  );
+}
+
+export default TodoForm;
